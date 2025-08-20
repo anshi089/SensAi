ee43781fec033162fd9c773af6daea10e5c7af74
anshi089 <anshikatomar656@gmail.com>
2025-08-21 02:27:10 +0530

    project completed

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -0,0 +1,11 @@
+
+import { SignIn } from '@clerk/nextjs';
+
+const Page = () => {
+  return <SignIn />;
+}
+
+export default Page
+// export default function Page() {
+//   return <SignIn />;
+// }
